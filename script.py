fa93e19388e7c2550f45be8231d9c497284e9a2e
Jon Kartago Lamida <jon.lamida@go-jek.com>
2020-04-22 16:09:34 +0800

    Update the script to use json database

--- script.py
+++ script.py
@@ -36,23 +36,119 @@ urls = [
     "https://bookdepository.com/Learning-Kali-Linux-Ric-Messier/9781492028697",
     "https://bookdepository.com/Terraform-Up-Running-Yevgeniy-Brikman/9781492046905",
     "https://bookdepository.com/Fundamentals-Software-Architecture-Mark-Richards/9781492043454",
+    "https://www.amazon.sg/Fundamentals-Software-Architecture-Engineering-Approach/dp/1492043451",
 ]
 
-results = []
+class Product:
+    def __init__(self):
+        pass
 
-for url in urls:
-    html = urlopen(url)
+    def url(self, url):
+        self.url = url
+
+    def name(self, name):
+        self.name = name
+
+    def price(self, price):
+        self.price = price
+
+    def last_price(self, last_price):
+        self.last_price = last_price
+
+    def min_price(self, min_price):
+        self.min_price = min_price
+
+    def rrp(self, rrp):
+        self.rrp = rrp
+
+def get_bd_meta(url, html):
+    product = Product()
+    product.url(url)
+
     bs = BeautifulSoup(html.read(), 'html.parser')
-    prices = bs.findAll('span', {'class':'sale-price'})
-    rrps = bs.findAll('span', {'class':'rrp'})
-    names = bs.findAll('h1', {'itemprop':'name'})
-    product = {}
-    if len(names) > 0:
-        product["name"] = names[0].text
-    if len(prices) > 0:
-        product["price"] = prices[0].text
-    if len(rrps) > 0:
-        product["rrp"] = rrps[0].text
-    results.append(product)
+    prices = bs.findAll('span', {'class': 'sale-price'})
+    rrps = bs.findAll('span', {'class': 'rrp'})
+    names = bs.findAll('h1', {'itemprop': 'name'})
 
-print(json.dumps(results, sort_keys=True, indent=4, separators=(',', ': ')))
+    if len(names) > 0:
+        product.name(names[0].text)
+    if len(prices) > 0:
+        product.price(prices[0].text)
+    if len(rrps) > 0:
+        product.rrp(rrps[0].text)
+    return product
+
+def get_amazon_meta(url, html):
+    product = Product()
+    product.url(url)
+
+    bs = BeautifulSoup(html.read(), 'html.parser')
+    prices = bs.findAll('span', {'class': 'offer-price'})
+    names = bs.findAll(id='productTitle')
+
+    if len(names) > 0:
+        product.name(names[0].text)
+    if len(prices) > 0:
+        product.price(prices[0].text)
+    return product
+
+
+def get_product(url):
+    html = urlopen(url)
+    if "bookdepository" in url:
+        return get_bd_meta(url, html)
+    elif "amazon" in url:
+        return get_amazon_meta(url, html)
+    else:
+        print(f"Unknown provider {url}")
+        return None
+
+STORAGE = "books.json"
+
+def run():    
+    db = dict()
+    try:
+        with open(STORAGE) as storage:
+            json_string = storage.read()
+            json_products = json.loads(json_string)
+
+            for url in sorted(json_products.keys()):
+                p = json_products[url]
+
+                product = Product()
+                product.url(url)
+                product.name(p.get('name'))
+                product.price(p.get('price'))
+                product.last_price(p.get('last_price'))
+                product.min_price(p.get('min_price'))
+                product.rrp(p.get('rrp'))
+
+                db[p['url']] = product
+    except Exception as e:
+        print(f"Error {e}")
+        with open(STORAGE, "w") as storage:
+            print("The DB file is not available, create a new empty one")
+            storage.write("{}")
+
+    for url in urls:
+        print(f"Processing {url}")
+        stored_product = db.get(url)
+        product = get_product(url)
+        if stored_product == None:
+            print(f"No stored product for {url}")
+            product.min_price(product.price)
+            db[url] = product.__dict__
+        else:
+            print(f"Found stored product for {url}")
+            product.last_price(stored_product.price)
+            product.min_price(product.price if product.price < stored_product.min_price else stored_product.min_price)
+            db[url] = product.__dict__
+
+    updated_db_json = json.dumps(db, sort_keys=True, indent=4, separators=(',', ': '))
+    with open(STORAGE, 'w') as storage:
+        storage.write(updated_db_json)
+
+    print("Storing data...")
+    print(updated_db_json)
+
+run()
